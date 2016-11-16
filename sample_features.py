dec4a8e4a26d815184ec3762656b247f671b5e4f
W. Graham Mueller <wgmueller@gmail.com>
2016-11-16 12:29:51 -0500

    more synatx errors from translation

--- sample_features.py
+++ sample_features.py
@@ -1,29 +1,27 @@
 def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
 
-	num_accept = np.zeros([1,2])
-	num_prop = np.zeros([1,2])
+  num_accept = np.zeros([1,2])
+  num_prop = np.zeros([1,2])
 
-	obsModelType = obsModel['type']
-	priorType = obsModel['priorType']
+  obsModelType = obsModel['type']
+  priorType = obsModel['priorType']
+  numObj,Kz_prev = F_prev.shape
+  Kz_max = Kz_prev+numObj
+  F = np.zeros([numObj,Kz_max])
+  F[:,0:Kz_prev] = F_prev
+  F = np.where(F > 0)
+  featureCounts = np.sum(F,0)
+  stored_log_likelihood = np.zeros([1,numObj])
 
-	[numObj Kz_prev] = F_prev.shape
-	Kz_max = Kz_prev+numObj
-	F = np.zeros([numObj,Kz_max])
-	F[:,0:Kz_prev] = F_prev
-	F = np.where(F > 0)
-	featureCounts = np.sum(F,0)
-	stored_log_likelihood = np.zeros([1,numObj])
+  Ks = dist_struct[1]['pi_s'].shape[1]
 
-	Ks = dist_struct[1]['pi_s'].shape[1]
+  log_likelihood_ii_kk = np.array([0,0])
 
-	log_likelihood_ii_kk = np.array([0,0])
+  seq_order = randperm(numObj)
+  feature_inds = np.array(range(0,Kz_max))
 
-	seq_order = randperm(numObj)
-
-	feature_inds = np.array([0:Kz_max])
-
-	config_log_likelihood = 0
-	for ii in range(0,seq_order):
+  config_log_likelihood = 0
+  for ii in range(0,seq_order):
     
     # Reset vector indicating the previous set of unique features to object i:
     unique_features_ii = np.zeros([1,Kz_max])
@@ -31,12 +29,12 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
     
     # Compute likelihood under all possible parameters (including ones we
     # have not yet seen in the data):
-    log_likelihood = compute_likelihood_unnorm(data_struct[ii],theta,obsModelType,[0:Kz_max],Kz_max,Ks)
+    log_likelihood = compute_likelihood_unnorm(data_struct[ii],theta,obsModelType,range(0,Kz_max),Kz_max,Ks)
     dimu = data_struct[ii]['obs'].shape[1]
     
     # Compute current likelihood of the current feature assignments:
     if np.sum(F[ii,:]) == 0:
-        stored_log_likelihood(ii) = -np.inf
+      stored_log_likelihood[ii] = -np.inf
     else:
         
         pi_init = dist_struct[ii]['pi_init'][F[ii,:]]
@@ -56,9 +54,9 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
         fwd_msg,neglog_c = forward_message_vec(likelihood_ii,log_normalizer_ii,data_struct(ii).blockEnd,pi_z,pi_s,pi_init);
         
         if np.isnan(np.sum(neglog_c)):
-            stored_log_likelihood(ii) = -np.inf
+            stored_log_likelihood[ii] = -np.inf
         else:
-            stored_log_likelihood(ii) = np.sum(neglog_c) #observation_likelihood(F(ii,:),data_struct(ii),obsModelType,dist_struct(ii),theta);
+            stored_log_likelihood[ii] = np.sum(neglog_c) #observation_likelihood(F(ii,:),data_struct(ii),obsModelType,dist_struct(ii),theta);
         
 
 	    #For each of the currently instantiated features (this vector will
@@ -71,17 +69,17 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
 	        featureCounts[kk] = featureCounts[kk]-F[ii,kk]
 	        
 	        # If other objects are using this feature:
-	        if featureCounts(kk)>0
+	        if featureCounts(kk)>0:
 	                      
 	            # Grab out previous likelihood of data under this feature
 	            # assignment:
 	            log_likelihood_ii_kk[Fik_prev+1] = stored_log_likelihood[ii]
 	            
 	            # Try out other value for f_{ik}:
-	            F[ii,kk] = !Fik_prev 
+	            F[ii,kk] = not(Fik_prev)
 	            
 	            if np.sum(F[ii,:]) == 0:
-	                log_likelihood_ii_kk[!Fik_prev+1] = -np.inf
+	                log_likelihood_ii_kk[not(Fik_prev)+1] = -np.inf
 	            else:                
 	                pi_init = dist_struct[ii]['pi_init'][F[ii,:]]
 	                pi_init = pi_init/np.sum(pi_init)
@@ -100,9 +98,9 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
 	                fwd_msg,neglog_c = forward_message_vec(likelihood_ii,log_normalizer_ii,data_struct[ii]['blockEnd'],pi_z,pi_s,pi_init)
 	                
 	                if np.isnan(np.sum(neglog_c)):
-	                    log_likelihood_ii_kk(!Fik_prev+1) = -np.inf
+	                    log_likelihood_ii_kk[not(Fik_prev)+1] = -np.inf
 	                else:
-	                    log_likelihood_ii_kk(!Fik_prev+1) = np.sum(neglog_c) #observation_likelihood(F(ii,:),data_struct(ii),obsModelType,dist_struct(ii),theta);
+	                    log_likelihood_ii_kk[not(Fik_prev)+1] = np.sum(neglog_c) #observation_likelihood(F(ii,:),data_struct(ii),obsModelType,dist_struct(ii),theta);
 	             
 	            
 	            # Compute accept-reject ratio:
@@ -115,9 +113,9 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
 	            else:
 	                
 	                if rho>1:
-	                    F[ii,kk] = !Fik_prev
+	                    F[ii,kk] = not(Fik_prev)
 	                else:
-	                    sample_set = [Fik_prev,!Fik_prev]
+	                    sample_set = [Fik_prev,not(Fik_prev)]
 	                    ind = (random.random()>(1-rho))
 	                    F[ii,kk] = sample_set[ind]
 	          
@@ -143,13 +141,13 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
     #q = ones(1,num_unique_features+1);
     p_birth = 1-stats.poisson.cdf(num_unique_features,gamma0/numObj) #0.5; poisspdf(0,gamma0/numObj);
     q = ((1-p_birth)/num_unique_features)*np.ones([1,num_unique_features+1])
-    q(1) = p_birth
+    q[1] = p_birth
     q = q/np.sum(q)
     
     Q   = cumsum(q);
     transition_case = 1 + np.sum(Q[-1]*random.random() > Q)
    
-   if transition_case==1:
+    if transition_case==1:
        # Birth:
        f_ii = F[ii,:]
        f_ii[Kz_prev + ii] = 1
@@ -158,7 +156,7 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
        #q_rev = ones(1,num_new_unique_features+1);
        p_birth_rev = 1-stats.poisson.cdf(num_new_unique_features,gamma0/numObj) #0.5; poisspdf(0,gamma0/numObj);
        q_rev = ((1-p_birth_rev)/num_new_unique_features)*np.ones([1,num_new_unique_features+1])
-       q_rev(1) = p_birth_rev
+       q_rev[1] = p_birth_rev
        q_rev = q_rev/np.sum(q_rev)
        
        log_prob_proposal = math.log(q[0])  # probability of birth
@@ -167,7 +165,7 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
        num_prop[1] = num_prop[1]+1
        
        #print('propose birth')
-   else:
+    else:
        unique_feature_inds = feature_inds[unique_features_ii]
        death_ind = transition_case-1
        f_ii = F[ii,:]
@@ -177,22 +175,22 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
        #q_rev = ones(1,num_new_unique_features+1);
        p_birth_rev = 1-stats.poisson.cdf(num_new_unique_features,gamma0/numObj)
        q_rev = ((1-p_birth_rev)/num_new_unique_features)*np.ones([1,num_new_unique_features+1])
-       q_rev(1) = p_birth_rev
+       q_rev[1] = p_birth_rev
        q_rev = q_rev/np.sum(q_rev)
        
        log_prob_proposal = math.log(q[transition_case])  # probability of killing that feature
-       log_prob_reverse_proposal = math.log(q_rev[0] # probability of birth step
+       log_prob_reverse_proposal = math.log(q_rev[0]) # probability of birth step
        
        num_prop[0] = num_prop[0] + 1
        #print('propose death')
 
    # Grab likelihood under the previous assignment:
-   log_likelihood_ii_kk[0] = stored_log_likelihood[ii]
+    log_likelihood_ii_kk[0] = stored_log_likelihood[ii]
    
    # Compute likelihood under the proposed change:
-   if np.sum(f_ii) == 0:
+    if np.sum(f_ii) == 0:
        log_likelihood_ii_kk[1] = -np.inf
-   else:
+    else:
        pi_init = dist_struct[ii]['pi_init'][f_ii]
        pi_init = pi_init/np.sum(pi_init)
        pi_z = dist_struct[ii]['pi_z'][f_ii,f_ii]
@@ -203,7 +201,7 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
        # Pass messages forward to integrate over the mode/state sequence:
        log_likelihood_ii = log_likelihood[f_ii,:,:]
        log_normalizer_ii = np.max(np.max(log_likelihood_ii,axis=0),axis=1)
-       log_likelihood_ii = log_likelihood_ii - log_normalizer_ii(np.ones([np.sum(f_ii),1]),np.ones([Ks,1)],:)
+       log_likelihood_ii = log_likelihood_ii - log_normalizer_ii[np.ones([np.sum(f_ii),1]),np.ones([Ks,1]),:]
        likelihood_ii = np.exp(log_likelihood_ii)
        log_normalizer_ii = log_normalizer_ii - (dimu/2)*log(2*pi);\
        
@@ -216,15 +214,15 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
       
    
    # Compute accept-reject ratio:
-   log_rho_star = (log_likelihood_ii_kk[1] - log_likelihood_ii_kk[0])\
+    log_rho_star = (log_likelihood_ii_kk[1] - log_likelihood_ii_kk[0])\
        + (log(poisspdf(num_new_unique_features,gamma0/numObj)) - log(poisspdf(num_unique_features,gamma0/numObj)))\
        + (log_prob_reverse_proposal - log_prob_proposal);\
-   rho = exp(log_rho_star);\
+    rho = exp(log_rho_star);\
    
    # Sample new feature value:
-   if np.isnan(rho):
+    if np.isnan(rho):
        raise Exception('NaN rho')
-   else:
+    else:
        
        if rho>1:
            F[ii,:] = f_ii
@@ -249,12 +247,12 @@ def sample_features(F_prev,gamma0,data_struct,dist_struct,theta,obsModel):
 #        display('moved to higher likelihood')
 #    end
    
-   stored_log_likelihood[ii] = log_likelihood_ii_kk[ind+1]
+    stored_log_likelihood[ii] = log_likelihood_ii_kk[ind+1]
    
-   config_log_likelihood = config_log_likelihood + stored_log_likelihood[ii]
+    config_log_likelihood = config_log_likelihood + stored_log_likelihood[ii]
    
-   featureCounts = np.sum(F,axis=0)
-   F,dist_struct,theta = reallocate_states(F,dist_struct,theta,priorType);
-   return F,dist_struct,theta,config_log_likelihood,num_accept,num_prop 
+    featureCounts = np.sum(F,axis=0)
+    F,dist_struct,theta = reallocate_states(F,dist_struct,theta,priorType);
+    return F,dist_struct,theta,config_log_likelihood,num_accept,num_prop 
 
 	    
